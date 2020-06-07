78e42758d0063e9e01d8cb47c74ac979b91b1467
dhirajb7 <bond.dhiraj@gmail.com>
2020-06-07 07:25:34 +0530

    modal design started

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Modal from "react-modal";
 
-function AddUser() {
-  return <h1>ADD USER POP UP</h1>;
+Modal.setAppElement("#root");
+function AddUser(props) {
+  return (
+    <Modal isOpen={props.value}>
+      <h2>HEADING</h2>
+      <p>BODY</p>
+    </Modal>
+  );
 }
 
 export default AddUser;
--- src/Components/UserBar.js
+++ src/Components/UserBar.js
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import users from "../assets/ico_users.svg";
 import add from "../assets/ico_add.svg";
 import search from "../assets/ico_search.svg";
 import AddUser from "./AddUser";
 
-function UserBar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-white bg-white">
-      <div className="navbar-brand Users">
-        <img src={users} alt="users" /> Users
-      </div>
+class UserBar extends Component {
+  state = {
+    modalIsOpen: false,
+  };
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto"></ul>
-        <div className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-            aria-describedby="basic-addon2"
-          />
+  addUserClicked = () => {
+    this.setState({
+      modalIsOpen: true,
+    });
+  };
 
-          <img className="searchIcon" src={search} alt="search" />
+  render() {
+    return (
+      <React.Fragment>
+        <AddUser value={this.state.modalIsOpen} />
 
-          <button className="btn btn-outline-warning my-2 my-sm-0 addUser">
-            <img src={add} alt="add" /> Add User
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
+        <nav className="navbar navbar-expand-lg navbar-white bg-white">
+          <div className="navbar-brand Users">
+            <img src={users} alt="users" /> Users
+          </div>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto"></ul>
+            <div className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="basic-addon2"
+              />
+
+              <img className="searchIcon" src={search} alt="search" />
+
+              <button
+                className="btn btn-outline-warning my-2 my-sm-0 addUser"
+                onClick={this.addUserClicked}
+              >
+                <img src={add} alt="add" /> Add User
+              </button>
+            </div>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
 }
-
 export default UserBar;
